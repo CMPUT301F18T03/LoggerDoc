8919c441a53ce0c658f4a6a09fbf6e808beebb4f
Dylan <dyladeco@live.com>
2018-10-30 13:05:43 -0600

    Created some basic functionality unit tests for the Patient/Caregiver lists and for the Patient class. Didnt do one for Caregiver class as it felt redundant.

---
+++ app/src/test/java/com/example/loggerdoc/CaregiverUnitTest.java
@@ -0,0 +1,42 @@
+package com.example.loggerdoc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CaregiverUnitTest {
+
+    @Test
+    public void TestAddCaregiver() {
+        CareGiverList testList = new CareGiverList();
+        assertTrue("Size should be 0 since we have not added anything", testList.size() == 0);
+
+        CareGiver testCaregiver = new CareGiver("test", "test", "test", new PatientList());
+        testList.addCareGiver(testCaregiver);
+        assertTrue("Size should be 1", testList.size() == 1);
+
+        assertTrue("Does the list contain the object we added", testList.contains(testCaregiver));
+
+    }
+
+    @Test
+    public void TestRemoveCaregiver() {
+        CareGiverList testList = new CareGiverList();
+        assertTrue("Size should be 0 since we have not added anything", testList.size() == 0);
+
+        CareGiver testCaregiver = new CareGiver("test", "test", "test", new PatientList());
+        testList.addCareGiver(testCaregiver);
+        assertTrue("Size should be 1", testList.size() == 1);
+        assertTrue("Does the list contain the object we added", testList.contains(testCaregiver));
+
+        testList.removeCareGiver(testCaregiver);
+        assertTrue(testList.size() == 0);
+        assertFalse(testList.contains(testCaregiver));
+
+
+    }
+
+
+
+
+}
