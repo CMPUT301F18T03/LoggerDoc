361a2b2999ab40aff8a4bb26d7a02196630ed1df
MultiTrackDrifting <tmring2010@hotmail.com>
2018-11-01 15:39:44 -0600

    Add blank UI classes

---
+++ app/src/main/java/com/example/loggerdoc/ActivityCareGiverBrowsePatients.java
@@ -0,0 +1,13 @@
+package com.example.loggerdoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityCareGiverBrowsePatients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_care_giver_browse_patients);
+    }
+}
---
+++ app/src/main/java/com/example/loggerdoc/ActivityProblemMap.java
@@ -0,0 +1,13 @@
+package com.example.loggerdoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityProblemMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem_map);
+    }
+}
