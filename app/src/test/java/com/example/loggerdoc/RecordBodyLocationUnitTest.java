329319b3db89293a07b3b16f4abe4de820a4142d
Tristen Glover <tmg098@gmail.com>
2018-11-01 22:31:26 -0600

    removed tests

--- app/src/test/java/com/example/loggerdoc/RecordBodyLocationUnitTest.java
+++ app/src/test/java/com/example/loggerdoc/RecordBodyLocationUnitTest.java
@@ -6,16 +6,5 @@ import static java.lang.Boolean.TRUE;
 import static org.junit.Assert.assertEquals;
 
 public class RecordBodyLocationUnitTest {
-
-    @Test
-    public void TestBodyLocation(){
-        String side = "right";
-        String location = "leg";
-        Boolean front = TRUE;
-        RecordBodyLocation bLocation = new RecordBodyLocation(side, front, location);
-        assertEquals("side should be set", side, bLocation.Side);
-        assertEquals("the location should be set", location, bLocation.location);
-        assertEquals("the front should be set",front, bLocation.Front);
-
-    }
+    
 }
