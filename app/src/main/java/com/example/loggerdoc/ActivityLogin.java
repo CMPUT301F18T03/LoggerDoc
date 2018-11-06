84c4d4d0805ca7b8952f255e8c14f4d8f6acbadc
Dylan <dylandeco@live.com>
2018-11-05 20:49:04 -0700

    Got code to build and run without erroring out.

--- app/src/main/java/com/example/loggerdoc/ActivityLogin.java
+++ app/src/main/java/com/example/loggerdoc/ActivityLogin.java
@@ -1,7 +1,6 @@
 package com.example.loggerdoc;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +17,7 @@ public class ActivityLogin extends AppCompatActivity {
 
     }
 
+    // Temporary userlist
     UserList userList = new UserList();
 
     // If user hits the login button
