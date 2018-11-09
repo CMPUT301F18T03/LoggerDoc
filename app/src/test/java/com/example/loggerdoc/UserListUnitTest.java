594bdf7c9c7a1d844e2accc4ccc0cbe942c5858d
Dylan <dyladeco@live.com>
2018-11-09 14:26:49 -0700

    made a test to add a Patient object into the UserList. Trivial

--- app/src/test/java/com/example/loggerdoc/UserListUnitTest.java
+++ app/src/test/java/com/example/loggerdoc/UserListUnitTest.java
@@ -22,4 +22,33 @@ public class UserListUnitTest {
 
     }
 
+    @Test
+    public void TestAddPatient(){
+        UserList testList = UserListController.getUserList();
+        assertTrue("Size should be 0 since we have not added anything", testList.size() == 0);
+
+        User testPatient = new Patient("test", "test", "test", new CareGiverList());
+        testList.addUser(testPatient);
+        assertTrue("Size should be 1", testList.size() == 1);
+
+        assertTrue("Does the list contain the object we added", testList.containsUser(testPatient));
+
+    }
+
+    @Test
+    public void TestRemovePatient(){
+        UserList testList = UserListController.getUserList();
+        assertTrue("Size should be 0 since we have not added anything", testList.size() == 0);
+
+        User testPatient = new Patient("test", "test", "test", new CareGiverList());
+        testList.addUser(testPatient);
+        assertTrue("Size should be 1", testList.size() == 1);
+
+        assertTrue("Does the list contain the object we added", testList.containsUser(testPatient));
+
+        testList.removeUser(testPatient);
+        assertTrue("Size should be 0 since we have not added anything", testList.size() == 0);
+
+    }
+
 }
