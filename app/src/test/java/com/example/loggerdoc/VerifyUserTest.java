aeee4410fd4e7270c7ed4d8a8ddad29bc83e527a
Dylan <dylandeco@live.com>
2018-11-12 18:14:28 -0700

    changed tests to work with abstract user

--- app/src/test/java/com/example/loggerdoc/VerifyUserTest.java
+++ app/src/test/java/com/example/loggerdoc/VerifyUserTest.java
@@ -11,27 +11,23 @@ public class VerifyUserTest {
 
         UserList testList = UserListController.getUserList();
         assertTrue("Size should be 0 since we have not added anything", testList.size() == 0);
-        User testUser0 = new User("test0", "test", "test");
-        User testUser1 = new User("test1", "test", "test");
-        User testUser2 = new User("test2", "test", "test");
-        User testUser3 = new User("test3", "test", "test");
+        Patient testUser0 = new Patient("test0", "test", "test", new CareGiverList());
+        Patient testUser1 = new Patient("test1", "test", "test", new CareGiverList());
         testList.addUser(testUser0);
         testList.addUser(testUser1);
-        testList.addUser(testUser2);
-        testList.addUser(testUser3);
+
 
         assertTrue("Size should be 1", testList.size() == 4);
 
         for (User user : testList.getUsers()){
             if (user.getUserID() == "test2") {
-                assertTrue("For loop should be returning correct user", testUser2 == user);
+                assertTrue("For loop should be returning correct user", testUser1 == user);
                 break;
             }
         }
 
         testList.removeUser(testUser0);
         testList.removeUser(testUser1);
-        testList.removeUser(testUser2);
-        testList.removeUser(testUser3);
+
     }
 }
