86dac0bf2ed3430322f8b0d12c9077b3bcd9b4cb
Dylan <dylandeco@live.com>
2018-11-15 14:49:02 -0700

    Added javadoc to the User Class

--- app/src/main/java/com/example/loggerdoc/User.java
+++ app/src/main/java/com/example/loggerdoc/User.java
@@ -3,6 +3,8 @@ package com.example.loggerdoc;
 
 import java.io.Serializable;
 
+
+
 public class User implements Serializable {
 
     private String userID;
@@ -10,7 +12,13 @@ public class User implements Serializable {
     private String phoneNumber;
     private String role;
 
-
+    /**
+     *
+     * @param userID A unique username for the User that is at least 8 characters long
+     * @param emailAddress email address of the user
+     * @param phoneNumber Users phone number
+     * @param role Role that the user will take. Either a Patient or CareGiver
+     */
     public User(String userID, String emailAddress, String phoneNumber ,String role) {
 
         this.userID = userID;
@@ -19,35 +27,62 @@ public class User implements Serializable {
         this.role = role;
     }
 
+    /**
+     *
+     * @return Returns the users username
+     */
     public String getUserID() {
         return userID;
     }
 
+    /**
+     *
+     * @param userID New username that will replace the old userID
+     */
     public void setUserID(String userID) {
         this.userID = userID;
     }
 
+    /**
+     *
+     * @return Returns the users email address
+     */
     public String getEmailAddress() {
         return emailAddress;
     }
 
+    /**
+     *
+     * @param emailAddress new email address to change the old one
+     */
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
 
+    /**
+     *
+     * @return Returns the users phone number as a String
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     *
+     * @param phoneNumber new phone number to change the old one
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+
+    /**
+     *
+     * @return Returns the tole that the user is. Either Patient or CareGiver
+     */
     public String getRole() {
         return role;
     }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
+
 }
