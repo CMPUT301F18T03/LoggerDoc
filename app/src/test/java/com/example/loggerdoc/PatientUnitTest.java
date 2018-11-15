799608d77057502b3cb419fab7cfe390d6643196
Dylan <dylandeco@live.com>
2018-11-15 00:40:55 -0700

    Fixed tests to have role attribute when making patients and caregivers

--- app/src/test/java/com/example/loggerdoc/PatientUnitTest.java
+++ app/src/test/java/com/example/loggerdoc/PatientUnitTest.java
@@ -9,15 +9,15 @@ public class PatientUnitTest {
 
     @Test
     public void CreatePatientTest() {
-        Patient patient1 = new Patient("test1", "test1", "test1", new CareGiverList());
-        Patient patient2 = new Patient("test1", "test1", "test1", new CareGiverList());
+        Patient testPatient1 = new Patient("test", "test", "test","test", new CareGiverList());
+        Patient testPatient2 = new Patient("test", "test", "test","test", new CareGiverList());
 
-        assertNotEquals("Check to see if the Patient objects are different", patient1, patient2);
+        assertNotEquals("Check to see if the Patient objects are different", testPatient1, testPatient2);
     }
 
     @Test
     public void PatientUserIDTest() {
-        Patient testPatient = new Patient("test", "test@ualberta.ca", "123455678", new CareGiverList());
+        Patient testPatient = new Patient("test", "test@ualberta.ca", "123455678","test", new CareGiverList());
         assertEquals("test", testPatient.getUserID());
 
         testPatient.setUserID("changing user ID");
