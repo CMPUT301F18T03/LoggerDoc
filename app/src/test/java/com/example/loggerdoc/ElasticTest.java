663d8d0f9b9795869ea8dc61e66e41d86943f745
Drifter <tmring2010@hotmail.com>
2018-11-17 13:39:30 -0700

    a working test

--- app/src/test/java/com/example/loggerdoc/ElasticTest.java
+++ app/src/test/java/com/example/loggerdoc/ElasticTest.java
@@ -2,26 +2,29 @@ package com.example.loggerdoc;
 
 import android.util.Log;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.TreeMap;
 import java.util.concurrent.ExecutionException;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class ElasticTest {
-    @Test
-    public void TestElasticsearchConnection() {
-        ElasticClient testclient = ElasticSearchController.getClient();
-        //String vnum = testclient.getElasticVersion();
-        //assertTrue(vnum != null);
+    private httphandler handler;
+    @Before
+    public void init(){
+        handler = new httphandler(ElasticSearchController.getHttpClient(),"http://104.237.6.208:9200");
     }
     @Test
     public void Testput(){
-        ElasticClient testclient = ElasticSearchController.getClient();
-        System.out.println(testclient.getrecord("_doc/2"));
-        System.out.println(testclient.rawhttpPUT("_doc/2","{\"name\": \"Jane Doe\"}"));
-        System.out.println(testclient.getrecord("_doc/2"));
+        handler.httpDELETE("/test/");
+        assertEquals("{\"error\":{\"root_cause\":[{\"type\":\"index_not_found_exception\",\"reason\":\"no such index\",\"resource.type\":\"index_expression\",\"resource.id\":\"test\",\"index_uuid\":\"_na_\",\"index\":\"test\"}],\"type\":\"index_not_found_exception\",\"reason\":\"no such index\",\"resource.type\":\"index_expression\",\"resource.id\":\"test\",\"index_uuid\":\"_na_\",\"index\":\"test\"},\"status\":404}",handler.httpGET("/test/_doc/2"));
+        handler.httpPUT("/test/_doc/2","{\"name\": \"Jane Doe\"}");
+        assertEquals("{\"_index\":\"test\",\"_type\":\"_doc\",\"_id\":\"2\",\"_version\":1,\"found\":true,\"_source\":{\"name\": \"Jane Doe\"}}",handler.httpGET("/test/_doc/2"));
+        handler.httpDELETE("/test/_doc/2");
+        assertEquals("{\"_index\":\"test\",\"_type\":\"_doc\",\"_id\":\"2\",\"found\":false}",handler.httpGET("/test/_doc/2"));
 
         /*System.out.println(testclient.httpGET(""));
         System.out.println(testclient.httpGET("_doc/2"));
@@ -34,27 +37,4 @@ public class ElasticTest {
         System.out.println(testclient.httpDELETE("_doc/2"));
         System.out.println(testclient.httpGET("_doc/2"));*/
     }
-    /*@Test //This test cannot work, it relies on android
-    public void Testuserlist(){
-        ElasticClient testclient = ElasticSearchController.getClient();
-        UserList x = UserListController.getUserList();
-        x.addUser(new CareGiver("asdfghjkl","1@3.ca","12312","farmer"));
-        x.addUser(new CareGiver("asdfghjkl2","1@3.ca","12312","farmer"));
-        x.addUser(new CareGiver("asdfghjkl3","1@3.ca","12312","farmer"));
-        x.addUser(new CareGiver("asdfghjkl4","1@3.ca","12312","farmer"));
-        x.addUser(new Patient("qwerty1","4@3.ca","423423","nou"));
-        x.addUser(new Patient("qwerty2","4@3.ca","423423","nou"));
-        x.addUser(new Patient("qwerty3","4@3.ca","423423","nou"));
-        x.addUser(new Patient("qwerty4","4@3.ca","423423","nou"));
-        x.addUser(new Patient("qwerty5","4@3.ca","423423","nou"));
-        ElasticClient.uploadUsersTask upload =  new ElasticClient.uploadUsersTask();
-        upload.execute(x);
-        try {
-            upload.get();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }*/
 }
