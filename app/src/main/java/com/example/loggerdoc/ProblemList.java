2c359f2fc30b4a6b615919411080681a0adf09bc
MultiTrackDrifting <tmring2010@hotmail.com>
2018-11-26 20:27:10 -0700

    added a callback, will probably need later

--- app/src/main/java/com/example/loggerdoc/ProblemList.java
+++ app/src/main/java/com/example/loggerdoc/ProblemList.java
@@ -12,6 +12,7 @@ package com.example.loggerdoc;
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
 
+import com.example.loggerdoc.elasticclient.ElasticCallback;
 import com.example.loggerdoc.elasticclient.ElasticDataCallback;
 import com.example.loggerdoc.elasticclient.getProblemsTask;
 import com.example.loggerdoc.elasticclient.modifyProblemTask;
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 
 public class ProblemList extends GenericList<Problem> implements ElasticDataCallback<ArrayList<Problem>> {
 
+    ElasticCallback callback = null;
     protected void add(Problem data,Context context) {
         super.add_internal(data);
         new modifyProblemTask(context).execute(data);
@@ -32,10 +34,20 @@ public class ProblemList extends GenericList<Problem> implements ElasticDataCall
     public void download(Integer elasticID, Context context) {
         new getProblemsTask(context,this).execute(elasticID);
     }
+    public void download(Integer elasticID, Context context, ElasticCallback callback) {
+        download(elasticID,context);
+        this.callback = callback;
+    }
 
     public void dataCallBack(ArrayList<Problem> data) {
         super.load(data);
+        if(callback !=null){
+            callback.callBack("Problems");
+            callback=null;
+        }
     }
+
+
 }
 
 
