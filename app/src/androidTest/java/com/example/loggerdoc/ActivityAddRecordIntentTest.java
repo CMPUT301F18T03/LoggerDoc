a54e73a26084f1e4ea95fd7abd7e2f30485815fb
crash <snickerless1@shaw.ca>
2018-11-28 20:30:52 -0700

    fixed ActivityAddRecordIntentTest

--- app/src/androidTest/java/com/example/loggerdoc/ActivityAddRecordIntentTest.java
+++ app/src/androidTest/java/com/example/loggerdoc/ActivityAddRecordIntentTest.java
@@ -9,6 +9,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -17,6 +19,7 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.contrib.ActivityResultMatchers.hasResultCode;
 import static android.support.test.espresso.contrib.ActivityResultMatchers.hasResultData;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ActivityAddRecordIntentTest {
@@ -25,26 +28,23 @@ public class ActivityAddRecordIntentTest {
     public IntentsTestRule<ActivityAddRecord> intentsTestRule =
             new IntentsTestRule<>(ActivityAddRecord.class, false, false);
 
-    /*
     @Before
     // create mock intent with mock patient and mock problem
     public void before() {
-        Patient p = new Patient("Test Patient", "test@example.com", "555-555-1234", "Patient");
-        Problem pr = new Problem("Test Problem", new DatePickerFragment(), "Test Problem's Description");
-        p.getProblems().add(pr);
+        Patient p = new Patient("Patty2222", "testPatient@example.com", "555-123-4567", "Patient");
+        Problem pr = new Problem("Possible concussion", LocalDateTime.now(),
+                "From car accident", p.getElasticID());
+        ProblemRecordListController.getProblemList().add_internal(pr);
 
-        Intent intent = new Intent();
-        intent.putExtra("Patient", p);
-        intent.putExtra("Position", 0);
-        intent.putExtra("Flag", "a");
-        intentsTestRule.launchActivity(intent);
+        Intent i = new Intent();
+        i.putExtra("Problem", pr.getElasticID());
+        intentsTestRule.launchActivity(i);
     }
-    */
 
     @Test
-    public void TestResultFromActivity() {
-        String testRecordTitle = "Record Title";
-        String testRecordComment = "Record Comment";
+    public void TestAddRecord() {
+        String testRecordTitle = "Cracked face";
+        String testRecordComment = "Really hurts";
 
         onView(withId(R.id.record_title_text))
                 .perform(typeText(testRecordTitle), closeSoftKeyboard());
@@ -52,8 +52,9 @@ public class ActivityAddRecordIntentTest {
                 .perform(typeText(testRecordComment), closeSoftKeyboard());
         onView(withId(R.id.record_create_button))
                 .perform(click());
-        assertThat(intentsTestRule.getActivityResult(), hasResultCode(Activity.RESULT_OK));
-        assertThat(intentsTestRule.getActivityResult(),
-                hasResultData(IntentMatchers.hasExtraWithKey("Record")));
+
+        Record r = ProblemRecordListController.getRecordList().getArray().get(0);
+        assertTrue(r.getTitle().equals(testRecordTitle));
+        assertTrue(intentsTestRule.getActivity().isFinishing());
     }
 }
