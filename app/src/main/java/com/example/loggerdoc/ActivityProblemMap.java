b300121174999dd4d8814b98aa798c9b421c3496
atyrrell1 <tyrrell1@ualberta.ca>
2018-11-29 15:45:13 -0700

    Created a Record Map for all the records

--- app/src/main/java/com/example/loggerdoc/ActivityProblemMap.java
+++ app/src/main/java/com/example/loggerdoc/ActivityProblemMap.java
@@ -3,11 +3,19 @@ package com.example.loggerdoc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class ActivityProblemMap extends AppCompatActivity {
-
+public class ActivityProblemMap extends AppCompatActivity implements OnMapReadyCallback {
+    private ArrayList<Record> geoLocationArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +26,42 @@ public class ActivityProblemMap extends AppCompatActivity {
 
         Patient patient = (Patient) UserListController.getUserList().get(patientID);
 
-        ArrayList<RecordGeoLocation> geoLocationArrayList = new ArrayList<RecordGeoLocation>();
+         geoLocationArrayList = new ArrayList<Record>();
 
-        //for (Record record : ProblemRecordListController.getRecordList().get)
+        for (Record record : ProblemRecordListController.getRecordList().getArray()){
+            if (record.getRecordGeoLocation() != null){
+                geoLocationArrayList.add(record);
+            }
+        }
+
+        Button returnButton = (Button) findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        initializeProblemMap();
+    }
+
+    private void initializeProblemMap() {
+        //intialize the map object
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.problemMap);
+        mapFragment.getMapAsync(ActivityProblemMap.this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        GoogleMap problemMap = googleMap;
+        problemMap.getUiSettings().setZoomControlsEnabled(true);
+
+        for (Record geoLocation : geoLocationArrayList){
+            MarkerOptions marker = new MarkerOptions()
+                    .position(new LatLng(geoLocation.getRecordGeoLocation().getLatitude(),
+                            geoLocation.getRecordGeoLocation().getLongitude()))
+                    .title(geoLocation.getTitle());
+            problemMap.addMarker(marker);
+        }
     }
 }
