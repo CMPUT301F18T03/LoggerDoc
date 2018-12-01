174495cf2cf8a3779685e44409427339fbd0cccb
MultiTrackDrifting <tmring2010@hotmail.com>
2018-12-01 13:08:53 -0700

    descent into madness

--- app/src/main/java/com/example/loggerdoc/BodyLocationPhoto.java
+++ app/src/main/java/com/example/loggerdoc/BodyLocationPhoto.java
@@ -7,25 +7,10 @@ import com.example.loggerdoc.elasticclient.ElasticID;
 import java.io.File;
 import java.io.Serializable;
 
-public class BodyLocationPhoto implements Serializable,ElasticID {
+public class BodyLocationPhoto extends RecordPhoto implements Serializable,ElasticID {
 
-    private File photo;
     private String label;
-    private Integer ElasticID;
     public String getLabel(){ return this.label; }
     public void setLabel(String newlabel){this.label = newlabel;}
 
-    public File getPhoto() {
-        return this.photo;
-    }
-
-
-    public void setPhoto(File newPhoto) {
-        this.photo = newPhoto;
-    }
-
-    @Override
-    public Integer getElasticID() {
-        return ElasticID;
-    }
 }
--- app/src/main/java/com/example/loggerdoc/RecordPhoto.java
+++ app/src/main/java/com/example/loggerdoc/RecordPhoto.java
@@ -21,7 +21,9 @@ public class RecordPhoto implements Serializable,ElasticID {
         ElasticID = this.hashCode();
     }
 
+    public RecordPhoto(){
 
+    }
 
     /**
      *
