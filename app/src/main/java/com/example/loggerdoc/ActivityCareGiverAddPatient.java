3136497e16c47ae659251a6112d500d588ef953f
Dylan <dylandeco@live.com>
2018-12-02 18:34:04 -0700

    reeeee

--- app/src/main/java/com/example/loggerdoc/ActivityCareGiverAddPatient.java
+++ app/src/main/java/com/example/loggerdoc/ActivityCareGiverAddPatient.java
@@ -74,10 +74,11 @@ public class ActivityCareGiverAddPatient extends AppCompatActivity {
                 finish();
                 return;
             }
-        //user not found, display text and do nothing
-        Toast.makeText(this, "That username does not exist. Please try again.", Toast.LENGTH_SHORT).show();
-        userID.setText("");
-
+            else {
+                //user not found, display text and do nothing
+                Toast.makeText(this, "That username does not exist. Please try again.", Toast.LENGTH_SHORT).show();
+                userID.setText("");
+            }
         }
     }
 
